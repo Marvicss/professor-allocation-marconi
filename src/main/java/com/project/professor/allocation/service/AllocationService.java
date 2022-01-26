89932d43189c1200b4e53fa0c54b4ea6c2674cbe
Marconi Victor <marconevictor2014@gmail.com>
2022-01-26 19:32:49 -0300

    Criando as classes de serviço e construtores

---
+++ src/main/java/com/project/professor/allocation/service/AllocationService.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.service;
+import com.project.professor.allocation.repository.AllocationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AllocationService {
+
+    private AllocationRepository allocationRepository;
+
+    public AllocationService(AllocationRepository allocationRepository) {
+        this.allocationRepository = allocationRepository;
+    }
+}
