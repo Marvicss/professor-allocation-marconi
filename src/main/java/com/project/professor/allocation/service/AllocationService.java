56a949821e3da0f98fff1d1e5926c3b3b15fe822
Marconi Victor <marconevictor2014@gmail.com>
2022-01-26 20:44:45 -0300

    adicionando os metodos findById nas classas de serviço

--- src/main/java/com/project/professor/allocation/service/AllocationService.java
+++ src/main/java/com/project/professor/allocation/service/AllocationService.java
@@ -1,6 +1,9 @@
 package com.project.professor.allocation.service;
+import com.project.professor.allocation.entity.Allocation;
 import com.project.professor.allocation.repository.AllocationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AllocationService {
@@ -10,4 +13,13 @@ public class AllocationService {
     public AllocationService(AllocationRepository allocationRepository) {
         this.allocationRepository = allocationRepository;
     }
+
+    public Allocation findById(Long id){
+
+        Optional <Allocation> allocationOptional = allocationRepository.findById(id);
+        Allocation allocation = allocationOptional.orElse(null);
+        return allocation;
+
+    }
+
 }
--- src/main/java/com/project/professor/allocation/service/CourseService.java
+++ src/main/java/com/project/professor/allocation/service/CourseService.java
@@ -1,6 +1,10 @@
 package com.project.professor.allocation.service;
+import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.entity.Course;
 import com.project.professor.allocation.repository.CourseRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CourseService {
@@ -9,6 +13,16 @@ public class CourseService {
 
     public CourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
+    }
+
+
+    public Course findById(Long id){
+
+        Optional <Course> courseOptional = courseRepository.findById(id);
+        Course course = courseOptional.orElse(null);
+        return course;
+
+
     }
 }
 
